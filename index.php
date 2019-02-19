903b6eb2e28c53cbd92b5aaedda89fb6fee8aad5
Victor Lyamkin <Viktor.Lyamkin1.0@gmail.com>
2019-02-19 17:46:03 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+             <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+                         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+             <title>Document</title>
+</head>
+<body>
+
+    <form method="post" action="/script.php">
+        <div> Имя:
+            <input type="text" name="name">
+        </div><br>
+        <div> Телефон:
+            <input type="number" name="phone">
+        </div><br>
+        <div> Почта:
+            <input type="email" name="email">
+        </div><br>
+        <button type="submit">Отправить</button>
+
+
+</body>
+</html>
+
+
